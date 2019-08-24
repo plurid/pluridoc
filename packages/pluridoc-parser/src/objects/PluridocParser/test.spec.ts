77dc99291c6586bbf8ea414ce3be1233ca632a23
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-08-24 08:39:46 +0300

    pluridoc-parser > dev: PluridocParser

--- packages/pluridoc-parser/src/objects/PluridocParser/test.spec.ts
+++ packages/pluridoc-parser/src/objects/PluridocParser/test.spec.ts
@@ -64,4 +64,86 @@ plane content
         ];
         expect(pluridoc.getPlanesContent()).toStrictEqual(planesContent);
     });
+
+    it('reads multiline content', () => {
+        const text = `
+<<<
+plane content line one
+plane content line two
+    plane content indented
+>>>
+        `;
+        const pluridoc = new PluridocParser(text);
+        const planesContent = [
+            {
+                text: [
+                    'plane content line one',
+                    'plane content line two',
+                    '    plane content indented',
+                ],
+                metadata: {},
+            }
+        ];
+        expect(pluridoc.getPlanesContent()).toStrictEqual(planesContent);
+    });
+
+    it('reads plurid-plane content', () => {
+        const text = `
+<<<
+plane 1
+>>>
+
+<<<
+plane 2
+>>>
+        `;
+        const pluridoc = new PluridocParser(text);
+        const planesContent = [
+            {
+                text: [
+                    'plane 1',
+                ],
+                metadata: {},
+            },
+            {
+                text: [
+                    'plane 2',
+                ],
+                metadata: {},
+            },
+        ];
+        expect(pluridoc.getPlanesContent()).toStrictEqual(planesContent);
+    });
+
+    it('reads plurid-plane content', () => {
+        const text = `
+<<<
+plane 1a
+
+plane 1b
+>>>
+
+<<<
+plane 2
+>>>
+        `;
+        const pluridoc = new PluridocParser(text);
+        const planesContent = [
+            {
+                text: [
+                    'plane 1a',
+                    '',
+                    'plane 1b',
+                ],
+                metadata: {},
+            },
+            {
+                text: [
+                    'plane 2',
+                ],
+                metadata: {},
+            },
+        ];
+        expect(pluridoc.getPlanesContent()).toStrictEqual(planesContent);
+    });
 });
