b09c9c6aff9e2815e71857585ceef1423349eebe
Jan Cavel <caveljan@gmail.com>
2019-08-25 20:36:56 +0300

    pluridoc-servers > dev: basic serving

--- packages/pluridoc-server/src/objects/PluridocServer/index.ts
+++ packages/pluridoc-server/src/objects/PluridocServer/index.ts
@@ -13,18 +13,19 @@ import {
     DEFAULT_PLURID_PORT,
     PLURID_EXTENSION,
     PLURIDOC_EXTENSION,
+    FAVICON,
 } from '../../constants';
 
 
 
 
 class PluridocServer implements IPluridocServer {
-    private server: any;
+    private server: http.Server;
     private port: number = DEFAULT_PLURID_PORT;
     private verbose: boolean = false;
 
     constructor(options?: PluridocServerOptions) {
-        if (options && options.port) {
+        if (options) {
             if (options.port) {
                 this.port = options.port;
             }
@@ -35,7 +36,15 @@ class PluridocServer implements IPluridocServer {
         }
 
         this.server = http.createServer((req, res) => {
-            res.end('works');
+            console.log('request arrived for', req.url);
+
+            if (req.url === '/favicon.ico') {
+                res.writeHead(200, {'Content-Type': 'image/x-icon'} );
+                fs.createReadStream(FAVICON).pipe(res);
+                return;
+            }
+
+            this.handleFiles(req, res);
         });
     }
 
@@ -44,24 +53,7 @@ class PluridocServer implements IPluridocServer {
             console.log(`Server started on port ${this.port}`);
         }
 
-        this.server.listen(this.port, () => {
-            console.log(`--- Server started on port ${this.port}`);
-            console.log(process.cwd());
-            fs.readdirSync(process.cwd()).forEach(file => {
-                const extension = path.extname(file);
-                if (
-                    extension === PLURID_EXTENSION
-                    || extension === PLURIDOC_EXTENSION
-                ) {
-                    const text = fs.readFileSync(file, 'utf8');
-                    const pluridocParser = new PluridocParser(text);
-                    console.log('file', file);
-                    console.log('text', text);
-                    console.log('parsed:');
-                    console.log(pluridocParser.getPlanesContent());
-                }
-            });
-        });
+        this.server.listen(this.port);
     }
 
     public stop() {
@@ -70,6 +62,27 @@ class PluridocServer implements IPluridocServer {
         }
 
         this.server.close();
+    }
+
+    private handleFiles (req: any, res: any) {
+        fs.readdirSync(process.cwd()).forEach(file => {
+            const extension = path.extname(file);
+            if (
+                extension === PLURID_EXTENSION
+                || extension === PLURIDOC_EXTENSION
+            ) {
+                const text = fs.readFileSync(file, 'utf8');
+                const pluridocParser = new PluridocParser(text);
+                const content = pluridocParser.getPlanesContent();
+                if (req.url === '/' + file) {
+                    console.log(content);
+                    console.log(file);
+                    res.end(content[0].text[1]);
+                }
+            }
+        });
+
+        res.end('open a file');
     }
 }
 
