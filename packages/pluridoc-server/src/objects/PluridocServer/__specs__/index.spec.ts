8271eeec9a4e9849a945256ae6a4cb3fc186f490
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-08-25 11:45:24 +0300

    pluridoc-server > setup: basic

---
+++ packages/pluridoc-server/src/objects/PluridocServer/__specs__/index.spec.ts
@@ -0,0 +1,10 @@
+import PluridocServer from '../';
+
+
+
+describe('Pluridoc Server basic', () => {
+    it('works', () => {
+        const pluridocServer = new PluridocServer();
+        expect(true).toBe(true);
+    });
+});
