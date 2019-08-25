5bdac42b0d697f13baba4b91905faf90f4cfe00c
Jan Cavel <caveljan@gmail.com>
2019-08-25 19:16:19 +0300

    pluridoc-server > dev: PluridocServer options

--- packages/pluridoc-server/src/objects/PluridocServer/__specs__/index.spec.ts
+++ packages/pluridoc-server/src/objects/PluridocServer/__specs__/index.spec.ts
@@ -4,7 +4,11 @@ import PluridocServer from '../';
 
 describe('Pluridoc Server basic', () => {
     it('works', () => {
-        const pluridocServer = new PluridocServer();
+        const options = {
+            port: 3333,
+            verbose: true,
+        }
+        const pluridocServer = new PluridocServer(options);
         pluridocServer.start();
         pluridocServer.stop();
         expect(true).toBe(true);
