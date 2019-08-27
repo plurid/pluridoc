4851f531d6adf17c5c111c70cc6c50e8e33890b7
Jan Cavel <caveljan@gmail.com>
2019-08-27 11:19:07 +0300

    pluridoc-parser > setup: package, removeSpecs

---
+++ packages/pluridoc-parser/src/objects/PluridocParser/__specs__/linking.spec.ts
@@ -0,0 +1,24 @@
+import PluridocParser from '../';
+
+
+
+describe('Pluridoc Parser reads content', () => {
+    it.only('reads a basic link', () => {
+        const text = `
+<<<
+plane content '[linkName](linkTo)
+>>>
+        `;
+        const pluridoc = new PluridocParser(text);
+        const planesContent = [
+            {
+                text: [
+                    "plane content '[linkName](linkTo)",
+                ],
+                metadata: {},
+            }
+        ];
+        const result = pluridoc.getPlanesContent();
+        expect(result).toStrictEqual(planesContent);
+    });
+});
