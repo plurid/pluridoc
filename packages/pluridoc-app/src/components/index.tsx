8ea9df17a1ca2f36a250c71ed5834747ea587ad0
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-08-29 22:08:06 +0300

    pluridoc-app > dev: App, Dashboard, File

--- packages/pluridoc-app/src/components/index.tsx
+++ packages/pluridoc-app/src/components/index.tsx
@@ -8,9 +8,6 @@ import App from './App';
 const content = (window as any).__PLURIDOC_CONTENT__;
 const files = (window as any).__PLURIDOC_FILES__;
 
-console.log('bbb', content);
-console.log('ccc', files);
-
 const pluridocApp = document.getElementById('pluridoc-app');
 
 ReactDOM.render(
