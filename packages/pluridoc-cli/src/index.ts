393ad2cdc4b31ddbf14039c18af479e10f2dc985
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-08-29 19:28:11 +0300

    pluridoc-cli > dev: cli

--- packages/pluridoc-cli/src/index.ts
+++ packages/pluridoc-cli/src/index.ts
@@ -13,8 +13,8 @@ const pluridocServer = new PluridocServer(serverOptions);
 program
     .version('0.0.1', '-v, --version')
     .description("Create and edit .plurid and .pluridoc files.")
-    .option('-p, --plurid', 'create and edit a new .plurid file')
-    .option('-d, --pluridoc', 'create and edit a new .pluridoc file')
+    .option('-p, --plurid', 'create and edit a new .plurid file; specify <filename>')
+    .option('-d, --pluridoc', 'create and edit a new .pluridoc file; specify <filename>')
     .parse(process.argv);
 
 
@@ -22,10 +22,12 @@ if (process.argv.length === 2) {
     pluridocServer.start();
 }
 
-if (program.plurid) {
-    pluridocServer.newPlurid();
+if (process.argv.length <= 4 && program.plurid) {
+    const filename = process.argv[3] || 'newplurid';
+    pluridocServer.newPlurid(filename);
 }
 
-if (program.pluridoc) {
-    pluridocServer.newPluridoc();
+if (process.argv.length <= 4 && program.pluridoc) {
+    const filename = process.argv[3] || 'newpluridoc';
+    pluridocServer.newPluridoc(filename);
 }
