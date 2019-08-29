14ec8ccf1b8b21af512d794591ef6ddf54f4da13
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-08-29 14:16:14 +0300

    pluridoc-cli > dev: bin

--- packages/pluridoc-cli/src/index.ts
+++ packages/pluridoc-cli/src/index.ts
@@ -1,14 +1,30 @@
-// #!/usr/bin/env node
-
 import program from 'commander';
 
-import PluridocCLI from './objects/PluridocCLI';
+// import PluridocCLI from './objects/PluridocCLI';
+import PluridocServer from '@plurid/pluridoc-server';
 
-export default PluridocCLI;
+
+
+const pluridocServer = new PluridocServer();
 
 
 program
-    .version('0.0.1')
+    .version('0.0.1', '-v, --version')
     .description("Create and edit .plurid and .pluridoc files.")
     .option('-p, --plurid', 'create and edit a new .plurid file')
+    .option('-d, --pluridoc', 'create and edit a new .pluridoc file')
     .parse(process.argv);
+
+
+if (process.argv.length === 2) {
+    console.log('start server');
+    pluridocServer.start();
+}
+
+if (program.plurid) {
+    console.log('start server - create new .plurid');
+}
+
+if (program.pluridoc) {
+    console.log('start server - create new .pluridoc');
+}
