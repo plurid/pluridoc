51aa15429ab53895e63b0bd3a30efdd03785f34e
Jan Cavel <caveljan@gmail.com>
2019-08-29 13:41:40 +0300

    pluridoc-cli > setup: basic

---
+++ packages/pluridoc-cli/src/constants/index.ts
@@ -0,0 +1,2 @@
+export const PLURID_EXTENSION = '.plurid';
+export const PLURIDOC_EXTENSION = '.pluridoc';
---
+++ packages/pluridoc-cli/src/objects/PluridocCLI/index.ts
@@ -0,0 +1,20 @@
+// import PluridocServer from '@plurid/pluridoc-server';
+
+import {
+    IPluridocCLI,
+} from '../../interfaces';
+
+// import {
+//     PLURID_EXTENSION,
+//     PLURIDOC_EXTENSION,
+// } from '../../constants';
+
+
+
+class PluridocCLI implements IPluridocCLI {
+    constructor() {
+    }
+}
+
+
+export default PluridocCLI;
