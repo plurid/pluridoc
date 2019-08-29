29b46190718b3bd33a79f6052f28ffae91a08bbb
Jan Cavel <caveljan@gmail.com>
2019-08-29 21:59:48 +0300

    pluridoc-app > dev: PluridocApp

--- packages/pluridoc-app/src/components/App/index.tsx
+++ packages/pluridoc-app/src/components/App/index.tsx
@@ -19,30 +19,32 @@ const App: React.FC<AppOwnProperties> = (properties) => {
         files,
     } = properties;
 
+    console.log(content, files);
+
     if (!content) {
         return (
             <Dashboard
                 files={files}
             />
         );
+    } else {
+        return (
+            <div>
+                {content && content.map((planeContent: any) => {
+                    return (
+                        <div
+                            key={Math.random()*10000 + ''}
+                            className="plurid-plane"
+                        >
+                            <Editor
+                                content={planeContent}
+                            />
+                        </div>
+                    );
+                })}
+            </div>
+        );
     }
-
-    return (
-        <div>
-            {content && content.map((planeContent: any) => {
-                return (
-                    <div
-                        key={Math.random()*10000 + ''}
-                        className="plurid-plane"
-                    >
-                        <Editor
-                            content={planeContent}
-                        />
-                    </div>
-                );
-            })}
-        </div>
-    );
 }
 
 
