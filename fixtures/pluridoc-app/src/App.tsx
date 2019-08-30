22f0c1350b23f56262349a07d74f392f7b7b189e
Jan Cavel <caveljan@gmail.com>
2019-08-30 13:24:30 +0300

    pluridoc-app > dev: App > Dashboard

--- fixtures/pluridoc-app/src/App.tsx
+++ fixtures/pluridoc-app/src/App.tsx
@@ -8,6 +8,20 @@ const content: any[] = [];
 const files: any[] = [
     'a-file.plurid',
     'another-file.pluridoc',
+    'a-file2.plurid',
+    'another-file2.pluridoc',
+    'a-file3.plurid',
+    'another-file3.pluridoc',
+    'a-file4.plurid',
+    'another-file4.pluridoc',
+    'a-file5.plurid',
+    'another-file5.pluridoc',
+    'a-file6.plurid',
+    'another-file6.pluridoc',
+    'a-file7.plurid',
+    'another-file7.pluridoc',
+    'a-file8.plurid',
+    'another-file8.pluridoc',
 ];
 
 const App: React.FC = () => {
