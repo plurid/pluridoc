bef7204e94b512474d60bc57f5253943602be6dc
Jan Cavel <caveljan@gmail.com>
2019-09-22 10:08:15 +0300

    pluridoc-app > dev: PluridocApp

--- packages/pluridoc-app/src/objects/PluridocApp/index.tsx
+++ packages/pluridoc-app/src/objects/PluridocApp/index.tsx
@@ -20,7 +20,7 @@ class PluridocApp implements IPluridocApp {
     }
 
     render () {
-        const title = this.filename.slice(1,) || 'pluridoc';
+        const title = this.filename.slice(1,) + ' | pluridoc' || 'pluridoc';
 
         const clientScriptPath = path.join(__dirname, '../src/client/script.js');
         const clientScript = fs.readFileSync(clientScriptPath, 'utf8');
